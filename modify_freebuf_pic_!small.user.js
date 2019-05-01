3e30750f9827fd3a3e8a50939ad0dc21e1a2a307
东方有鱼名为咸 <Mr-xn@users.noreply.github.com>
2019-05-01 21:02:05 +0800

    Update modify_freebuf_pic_!small.user.js
    修复之前的滚动BUG，在文章页面禁用 lazyload

--- modify_freebuf_pic_!small.user.js
+++ modify_freebuf_pic_!small.user.js
@@ -2,32 +2,95 @@
 // @name         freebuf文章图片去除!small
 // @icon         https://static.freebuf.com/images/favicon.ico
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Remove the !small appended to the end of the image in the article section of freebuf.com and let the image display the best size instead of the reduced version. You don't need to click to zoom in to view the article.
 // @author       Mrxn
 // @homepage     https://mrxn.net/
 // @supportURL   https://github.com/Mr-xn/modify_freebuf_pic
 // @license      MIT
 // @run-at       document-end
-// @match        https://www.freebuf.com/*
+// @include      http://
+// @include      https://
+// @match        https://www.freebuf.com/*/*.html
 // @grant        unsafeWindow
 // ==/UserScript==
+//thanks https://userscripts-mirror.org/scripts/review/362011
 
-//添加按钮
-window.onload = function(){
-    var li = document.createElement("li");
-    li.id = "fuck_small";
-    li.class = "fuck_small";
-    var node = document.getElementById("scrollUp");
-    var pafk = node.parentNode;
-    pafk.appendChild(li);
-    var fktxt = document.getElementById("fuck_small");
-    fktxt.innerHTML = '<a href="javascript:;" onclick="fk_small();" title="去掉!small" style="background: rgba(0, 0, 0, 0.1); color: rgb(233, 25, 25); padding: 5px; font: 600 1em normal;"><span>Fuck<br>!small</span></a>';
-}();
-//清除图片后面带的 !small ，使图片可以按照原来大小显示，特别是一些大图片看不清，去掉后不需要
- document.getElementById('fuck_small').addEventListener( 'click',function fk_small(){
-     var fb_html_content = document.getElementById('contenttxt');
-     fb_html_content.innerHTML = fb_html_content.innerHTML.replace(/(!small)/gi,'');
+//start function
+function contentEval(source) {
+    if ('function' == typeof source) {
+        source = '(' + source + ')();'
+    }
+    var script = document.createElement('script');
+    script.setAttribute("type", "application/javascript");
+    script.textContent = source;
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+}
+
+contentEval(function(){
+var tma = 300; //运行间隔(毫秒)
+
+//禁止文章页面使用lazyload功能,因为使用了lazyload后，在滚动加载的过程种有冲突，还没有解决，暂时在文中页面禁用！望有能力搞的朋友不吝赐教!
+function nolazyload()
+{
+    try
+    {
+        var fb_content =document.getElementById("contenttxt");
+        var imgs =fb_content.getElementsByTagName("img");
+        // var imgs = document.images;
+        if(imgs.length==0) //页面加载完一个图都没有就跳出
+        {
+            //alert(document.location.href+"nopics");
+            return;
+        }
+        for(var i=0;i<imgs.length;i++)
+        {
+            if(pic_url_check(imgs[i].getAttribute("data-original"))&&imgs[i].src!=imgs[i].getAttribute("data-original"))
+            {
+                if(window.location.href.indexOf("image.3001.net")>=0)
+                {
+                    continue;
+                }
+                //imgs[i].src="";
+                imgs[i].src=imgs[i].getAttribute("data-original");
+            }
+        }
+
+    //alert("done"+"\n"+window.location.href);
+    }catch(e)
+    {
+        alert(e+"\n请将此错误信息发送至\n\nE-mail: admin@mrxn.net");
+    }
+
+    setTimeout(nolazyload,tma);
+}
+function pic_url_check(picx)
+{
+    if(!picx)return false;
+    if(picx.length>10)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//在滚动加载的时候移除图片!small后缀,修改文章图片width属性为100%.
+function new_img() {
+window.addEventListener('scroll',function(){
+     var fb_content =document.getElementById("contenttxt");
+     var imgs =fb_content.getElementsByTagName("img");
+     for (var i=0;i<imgs.length;i++){
+         // imgs[i].src==imgs[i].parentNode.href;
+         imgs[i].style.width="100%";
+     }
+     fb_content.innerHTML = fb_content.innerHTML.replace(/(!small)/gi,'');
      console.log("移除!small成功！");
-     //document.getElementById('fuck_small').removeEventListener('click',function fk_small(){});
-},true)
+     },false);
+}
+nolazyload();
+new_img();
+})();
